cd436bcfba07456b2f5903d16e2dd1528379b310
yshalsager <ysh-alsager@hotmail.com>
2022-08-07 09:33:08 +0200

    Update scraper
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- arcore_tracker.py
+++ arcore_tracker.py
@@ -4,15 +4,16 @@
 import difflib
 from datetime import date
 from os import environ, rename, path, system
+from time import sleep
 
+import pandas as pd
 from bs4 import BeautifulSoup
 from requests import get, post
-import pandas as pd
 
 TODAY = str(date.today())
 TG_CHAT = "@ARCoreDevices"
-BOT_TOKEN = environ['BOTTOKEN']
-GIT_OAUTH_TOKEN = environ['GIT_OAUTH_TOKEN_XFU']
+BOT_TOKEN = environ["BOTTOKEN"]
+GIT_OAUTH_TOKEN = environ["GIT_OAUTH_TOKEN_XFU"]
 CHANGES = []
 
 
@@ -21,25 +22,30 @@ def scrap_to_csv(url):
     fetches Google's page and scrape device's table to csv
     """
     response = get(url)
-    page = BeautifulSoup(response.content, 'html.parser')
-    tables = page.findAll("table")
+    page = BeautifulSoup(response.content, "html.parser")
+    tables = page.select("#google_play_devices ~ table")
     data = pd.read_html(str(tables), index_col=0)
-    open('devices.csv', 'w').close()
+    open("devices.csv", "w").close()
     for table in data:
-        [table.drop(columns=column, axis=1, inplace=True)
-         for column in table.columns if 'Notes' in column or 'Unnamed' in column]
-        with open('devices.csv', 'a') as out:
-            table.to_csv(out, sep=';', header=None)
+        [
+            table.drop(columns=column, axis=1, inplace=True)
+            for column in table.columns
+            if "Notes" in column or "Unnamed" in column
+        ]
+        with open("devices.csv", "a") as out:
+            table.to_csv(out, sep=";", header=None)
 
 
 def diff_files():
     """
     compare csv files to check for newly added devices
     """
-    with open('old.csv', 'r') as old, open('devices.csv', 'r') as new:
-        diff = difflib.unified_diff(old.readlines(), new.readlines(), fromfile='old', tofile='new')
+    with open("old.csv", "r") as old, open("devices.csv", "r") as new:
+        diff = difflib.unified_diff(
+            old.readlines(), new.readlines(), fromfile="old", tofile="new"
+        )
         for line in diff:
-            if line.startswith('+'):
+            if line.startswith("+"):
                 CHANGES.append(str(line))
 
 
@@ -47,20 +53,22 @@ def csv_to_md():
     """
     convert csv file to markdown
     """
-    with open('devices.csv', 'r') as csv_file:
+    with open("devices.csv", "r") as csv_file:
         csv = csv_file.readlines()
-    with open('README.md', 'w', encoding='UTF-8') as out:
-        out.write('# Google ARCore Supported devices\n')
-        out.write('Last sync is {}\n\nhttps://developers.google.com/ar/'
-                  'discover/supported-devices\n\n'.format(TODAY))
-        out.write('|Brand|Device|\n')
-        out.write('|---|---|\n')
+    with open("README.md", "w", encoding="UTF-8") as out:
+        out.write("# Google ARCore Supported devices\n")
+        out.write(
+            "Last sync is {}\n\nhttps://developers.google.com/ar/"
+            "discover/supported-devices\n\n".format(TODAY)
+        )
+        out.write("|Brand|Device|\n")
+        out.write("|---|---|\n")
         for line in csv:
             info = line.strip().split(";")
             try:
                 brand = info[0]
                 name = info[1]
-                out.write(f'|{brand}|{name}|\n')
+                out.write(f"|{brand}|{name}|\n")
             except IndexError:
                 pass
 
@@ -71,14 +79,16 @@ def post_to_tg():
     """
     for item in CHANGES[1:]:
         info = item.split(";")
-        brand = info[0].replace('+', '')
+        brand = info[0].replace("+", "")
         name = info[1].strip()
-        telegram_message = f"New ARCore device added!:\nBrand: *{brand}*\nName: *{name}*"
+        telegram_message = (
+            f"New ARCore device added!:\nBrand: *{brand}*\nName: *{name}*"
+        )
         params = (
-            ('chat_id', TG_CHAT),
-            ('text', telegram_message),
-            ('parse_mode', "Markdown"),
-            ('disable_web_page_preview', "yes")
+            ("chat_id", TG_CHAT),
+            ("text", telegram_message),
+            ("parse_mode", "Markdown"),
+            ("disable_web_page_preview", "yes"),
         )
         telegram_url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
         telegram_req = post(telegram_url, params=params)
@@ -87,25 +97,31 @@ def post_to_tg():
             print("{0}: Telegram Message sent".format(name))
         else:
             print("Telegram Error")
+        sleep(2)
 
 
 def git_commit_push():
     """
     git add - git commit - git push
     """
-    system("git add README.md devices.csv && git -c \"user.name=XiaomiFirmwareUpdater\" "
-       "-c \"user.email=xiaomifirmwareupdater@gmail.com\" commit -m \"[skip ci] sync: {0}\" && "" \
-       ""git push -q https://{1}@github.com/androidtrackers/arcore-devices.git HEAD:master"
-       .format(TODAY, GIT_OAUTH_TOKEN))
+    system(
+        'git add README.md devices.csv && git -c "user.name=XiaomiFirmwareUpdater" '
+        '-c "user.email=xiaomifirmwareupdater@gmail.com" commit -m "[skip ci] sync: {0}" && '
+        " \
+       "
+        "git push -q https://{1}@github.com/androidtrackers/arcore-devices.git HEAD:master".format(
+            TODAY, GIT_OAUTH_TOKEN
+        )
+    )
 
 
 def main():
     """
     ARCore supported devices tracker and scraper
     """
-    if path.exists('devices.csv'):
-        rename('devices.csv', 'old.csv')
-    url = 'https://developers.google.com/ar/discover/supported-devices'
+    if path.exists("devices.csv"):
+        rename("devices.csv", "old.csv")
+    url = "https://developers.google.com/ar/devices"
     scrap_to_csv(url)
     diff_files()
     if CHANGES:
@@ -114,5 +130,5 @@ def main():
         git_commit_push()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
